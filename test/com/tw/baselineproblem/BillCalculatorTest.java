2539808411be9737284e1f6e5a24b75f12102a70
saisree <saisree@INsaisree.local>
2015-08-28 18:01:56 +0530

    modified intial spec

--- test/com/tw/baselineproblem/BillCalculatorTest.java
+++ test/com/tw/baselineproblem/BillCalculatorTest.java
@@ -6,10 +6,13 @@ import static org.junit.Assert.assertEquals;
 
 public class BillCalculatorTest {
     @Test
-    public void test(){
+    public void shouldReturnReceiptForBookWhichIsExempted(){
         BillCalculator billCalculator=new BillCalculator();
         String actual=billCalculator.receipt("1 book of 12.36");
-        assertEquals("1 book of 12.36",actual);
+        assertEquals("1 book of 12.36\n SalesTax:0.00\n Total :12.36 ",actual);
     }
+
+
+
 
 }
