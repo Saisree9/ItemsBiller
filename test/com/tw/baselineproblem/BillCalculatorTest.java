abda5c5a0aebbcd6c92bbef08afd2c092af3ec29
saisree <saisree@INsaisree.local>
2015-08-28 18:18:23 +0530

    failing spec for chocolate bar which is exempted

--- test/com/tw/baselineproblem/BillCalculatorTest.java
+++ test/com/tw/baselineproblem/BillCalculatorTest.java
@@ -6,13 +6,17 @@ import static org.junit.Assert.assertEquals;
 
 public class BillCalculatorTest {
     @Test
-    public void shouldReturnReceiptForBookWhichIsExempted(){
-        BillCalculator billCalculator=new BillCalculator();
-        String actual=billCalculator.receipt("1 book of 12.36");
-        assertEquals("1 book of 12.36\n SalesTax:0.00\n Total :12.36",actual);
+    public void shouldReturnReceiptForBookWhichIsExempted() {
+        BillCalculator billCalculator = new BillCalculator();
+        String actual = billCalculator.receipt("1 book of 12.36");
+        assertEquals("1 book of 12.36\n SalesTax:0.00\n Total :12.36", actual);
     }
 
-
-
+    @Test
+    public void shouldReturnReceiptForChocolateBarWhichIsExempted(){
+        BillCalculator billCalculator=new BillCalculator();
+        String actual =billCalculator.receipt("1 chocolate bar at 0.85");
+        assertEquals("1 chocolate bar at 0.85\n SalesTax:0.00\n Total :0.85",actual);
+    }
 
 }
